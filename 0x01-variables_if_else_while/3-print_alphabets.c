94d3a373f58e02c21864bd8def4bb9e2535ba7c8
mekha2010 <abdomikha2010@gmail.com>
2023-03-20 19:33:05 -0700

    l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,17 @@
 
 int main(void)
 {
+	char ch = 'a';
 	char CH = 'A';
-	
+
+	/* print a - z */
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/* print A - Z */
 	while (CH <= 'Z')
 	{
 		putchar(CH);
